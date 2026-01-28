d1a3d523f3001a5ac9f3361d3b53a8516b0e22e9
li888 <alexlibaobao@gmail.com>
2026-01-28 15:00:26 -0500

    Initial commit

---
+++ src/test/java/org/example/TaskATest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordCountTest {
+
+    @Test
+    public void debug() throws Exception {
+        String[] input = new String[2];
+
+        /*
+        1. put the data.txt into a folder in your pc
+        2. add the path for the following two files.
+            windows : update the path like "file:///C:/Users/.../projectDirectory/data.txt"
+            mac or linux: update the path like "file:///Users/.../projectDirectory/data.txt"
+        */
+
+        input[0] = "file:///Users/alexli/CS4433/HW 1/HW1/src/access_logs.csv";
+        input[1] = "file:///Users/alexli/CS4433/HW 1/HW1/src/friends.csv";
+        input[2] = "file:///Users/alexli/CS4433/HW 1/HW1/src/pages.csv";
+
+        WordCount wc = new WordCount();
+        wc.debug(input);
+    }
+}
